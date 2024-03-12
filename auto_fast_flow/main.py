ebcb3b9f60f2286e7fbc7475628109312c44eeee
rourabpaul1986 <62745577+rourabpaul1986@users.noreply.github.com>
2024-03-12 15:59:58 +0100

    Update main.py
    pass proc_flow through command line argument

--- auto_fast_flow/main.py
+++ auto_fast_flow/main.py
@@ -1,17 +1,26 @@
 import csv
 import os
-
+import time
+import sys
 from py_lib.code_gen import *
 from py_lib.check_rule import *
 from py_lib.csv_lib import *
 ###############################################################################################
+
+if len(sys.argv) != 5:
+    print("Usage: python main.py c n m proc_file.csv where n=stream size and m=no.of stream & path of proc_file.csv")
+    sys.exit(1)
+c = sys.argv[1]
+n = sys.argv[2]
+m = sys.argv[3] 
+proc_file  = sys.argv[4] 
 defualt_dev_id= 0
 max_krnl=4
 num_devices=2
 arm_length=4 # [fpga, src, krnl, dst]
 fpga_column=0 # [fpga column in proc flow]
 ckt_file = 'input_file/circuit.csv'
-proc_file = 'input_file/proc_flow.csv'
+#proc_file = 'input_file/proc_flow1.csv'
 param_path = '../fpga/param.h'
 host_file = 'host.cpp'
 ###############################################################################################
@@ -27,8 +36,9 @@ with open('./py_lib/header', 'r') as file1:
 with open('./py_lib/pre_main', 'r') as file1:
     pre_main = file1.read()        
 with open('./py_lib/trail_main', 'r') as file1:
-    trail_main = file1.read()        
-
+    trail_main = file1.read()    
+print("-------------start---------------------")        
+start = time.time()
 with open(host_file, 'w') as output_file:
     
     output_file.write(header)    
@@ -41,9 +51,9 @@ get_device(host_file, req_fpga )
   
 unique_farms = find_unique_farms(proc_file)
 
-print("----------------------------------") 
-print("The number of farms is : "+str(len(unique_farms))) 
-print("emitter, collector, #worker") 
+#print("----------------------------------") 
+#print("The number of farms is : "+str(len(unique_farms))) 
+#print("emitter, collector, #worker") 
 ###############################################################################################
 #################################dynamic code writing portion##################################
 ###############################################################################################
@@ -62,6 +72,11 @@ with open(host_file, 'a') as output_file:
     output_file.write(trail_main)   
 
 output_file.close()
+
+
+end = time.time()
+print(end - start)
 ################# Turn on to run on FPGA#######################
-os.system("make test_host")
+command = f"make test_host c={c} n={n} m={m}"
+os.system(command)
 ###############################################################
